c73d129bff815702eacda4fc2126864dad83e865
muhammed.cansoy <cansoymuhammed@hotmail.com>
2022-10-22 17:42:03 +0300

    facingMode environment

--- js/media.js
+++ js/media.js
@@ -14,7 +14,19 @@ takeSnap.style.display="none"
 ////////////////////////////////////////////////////////////////
 const openCamera=async()=>{
     const constraints={
-        video:{width:300,height:300},
+        video: {
+            width: {
+              min: 1280,
+              ideal: 1920,
+              max: 2560,
+            },
+            height: {
+              min: 720,
+              ideal: 1080,
+              max: 1440
+            },
+            facingMode: 'environment'
+          },
         audio:true
     }
     const stream=await navigator.mediaDevices.getUserMedia(constraints)
